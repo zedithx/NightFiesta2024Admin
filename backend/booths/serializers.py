8f92cfdfb98977899340f67b245cd6852c48fd4c
Yang Si Jun <aersijun@gmail.com>
2024-02-16 01:51:51 +0800

    updated 8 digit rfid

--- backend/booths/serializers.py
+++ backend/booths/serializers.py
@@ -4,9 +4,13 @@ from .models import Player
 
 
 class PointSerializers(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+
     class Meta:
         model = Player
         fields = ('id', 'name', 'score')
-
+        
+    def get_id(self, obj):
+        return obj.id[:8]
 
 
